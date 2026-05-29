adce36fd148dd5665c762fd5d92af3fb3b27e37d
Primary17 <207167303+Primary17@users.noreply.github.com>
2026-05-29 12:47:37 +0300

    fix: correct media-files upload on heroku

--- restaurant_site/menu/api/urls.py
+++ restaurant_site/menu/api/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from .views import CategoryTreeView, DishDetailView, DishListView
 
@@ -6,4 +8,5 @@ urlpatterns = [
     path('categories/', CategoryTreeView.as_view(), name='menu-categories'),
     path('dishes/', DishListView.as_view(), name='menu-dishes'),
     path('dishes/<int:pk>/', DishDetailView.as_view(), name='menu-dish-detail'),
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 ]
